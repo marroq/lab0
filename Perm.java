ce96ba57554b65a7d6f21a3ea870cb269d604bc6
marroq <dffigueroa@galileo.edu>
2017-01-28 21:04:45 -0600

    Ejercicio 4

--- Perm.java
+++ Perm.java
@@ -3,9 +3,30 @@ public class Perm {
     // Asuma que todos los parametros son validos
     public static String permutation(String message, int spaces) {
         // Su codigo aqui
-        return "";
-    }
-
+        String alfa[] = new String[] {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+        String alfabeth = "abcdefghijklmnopqrstuvwxyz";
+        
+        String fVal="";
+        
+        for (int i=0; i<message.length(); i++) {
+            int index = alfabeth.indexOf(message.substring(i,i+1));
+            
+            if (spaces+index < 26) {
+                fVal += alfa[index+spaces];    
+            } else {
+                int perm = index+spaces;
+                while (perm >= 26) {
+                    perm -= 26;
+                    if (perm < 26) {
+                        fVal += alfa[perm];
+                    }
+                }
+            }
+        }
+        
+        return fVal;
+    }   
+    
     // NO MODIFICAR A PARTIR DE AQUI
 
     public static void main(String[] args) {
@@ -17,5 +38,4 @@ public class Perm {
             System.err.println("No se paso ningun argumento");
         }
     }
-
 }
