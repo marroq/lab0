a2bca7ec3adef8335d2efdd17e45e9cba321bbfa
marroq <dffigueroa@galileo.edu>
2017-01-28 19:36:33 -0600

    Ejercicio 2 terminado

--- Fibonacci.java
+++ Fibonacci.java
@@ -2,9 +2,19 @@ public class Fibonacci {
 
     public static int fibonacci(int n) {
         // Su codigo aqui
-        return 0;
+        if (n>1){
+           return fibonacci(n-1) + fibonacci(n-2); 
+        }
+        else if (n==1) {
+            return 1;
+        }
+        else if (n==0){
+            return 0;
+        } else {
+            return -1; 
+        }
     }
-
+    
     // NO MODIFICAR A PARTIR DE AQUI
 
     public static void main(String[] args) {
